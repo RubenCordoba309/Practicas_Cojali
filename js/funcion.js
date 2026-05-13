a1aaa2e4a30fe5361ea230ebef167b1acba5fee7
RubenCordoba <cordobalopezdepabloruben@gmail.com>
2026-05-13 09:40:52 +0200

    HTML, CSS y JS actualizados con apartado Estudios

--- js/funcion.js
+++ js/funcion.js
@@ -19,3 +19,41 @@ btn.addEventListener("click", () => {
         btn.textContent = "🌙";
     }
 });
+
+const estudios = [
+    {
+        titulo: "Grado Superior DAM",
+        centro: "IES Juan Bosco",
+        fecha: "2025 - presente"
+    },
+    {
+        titulo: "Bachillerato Tecnológico",
+        centro: "IES Eladio Cabañero",
+        fecha: "2022 - 2024"
+    },
+    {
+        titulo: "ESO",
+        centro: "IES Eladio Cabañero",
+        fecha: "2018 - 2022"
+    }
+];
+
+function renderizarEstudios() {
+    const contenedor = document.getElementById("lista-estudios");
+
+    contenedor.innerHTML = "";
+
+    estudios.forEach(estudio => {
+        const card = document.createElement("article");
+        card.className = "estudio-card";
+
+        card.innerHTML = `
+            <h4>${estudio.titulo}</h4>
+            <p><strong>${estudio.centro}</strong></p>
+            <p>${estudio.fecha}</p>
+        `;
+        contenedor.appendChild(card);
+    });
+}
+
+renderizarEstudios();
